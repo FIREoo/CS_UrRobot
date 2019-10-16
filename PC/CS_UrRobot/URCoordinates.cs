c425bed1c925c272692178099d40b003c4852952
FIRE <barry7577@gmail.com>
2019-10-16 15:03:53 +0800

    Initial commit

---
+++ PC/CS_UrRobot/URCoordinates.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrRobot.Coordinates
+{
+    static class ex
+    {
+        static public Unit M(this double value)
+        {
+            return new Unit(value);
+        }
+    }
+    public class Unit
+    {
+        double _M = 0;
+        public Unit(double v)
+        {
+            _M = v;
+        }
+        public Unit()
+        {
+            _M = 0;
+        }
+
+        public double M
+        {
+            get { return _M; }
+            set { _M = value; }
+        }
+        public double mm
+        {
+            get { return _M * 1000.0f; }
+            set { _M = value / 1000.0f; }
+        }
+    }
+
+    public class URCoordinates
+    {
+       public Unit X = new Unit();
+        public Unit Y = new Unit();
+        public Unit Z = new Unit();
+        public Unit Rx = new Unit();
+        public Unit Ry = new Unit();
+        public Unit Rz = new Unit();
+        public byte Grip = 0;
+
+
+        public URCoordinates(Unit _x, Unit _y, Unit _z, Unit _Rx, Unit _Ry, Unit _Rz, byte _G = 0)
+        {
+            X = _x;
+            Y = _y;
+            Z = _z;
+            Rx = _Rx;
+            Ry = _Ry;
+            Rz = _Rz;
+            Grip = _G;
+        }
+        /// <summary>
+        /// Unit meter
+        /// </summary>
+        /// <param name="_x">axis x (unit meter)</param>
+        /// <param name="_y">axis y (unit meter)</param>
+        /// <param name="_z">axis z (unit meter)</param>
+        /// <param name="_Rx">axis Rx (unit meter)</param>
+        /// <param name="_Ry">axis Ry (unit meter)</param>
+        /// <param name="_Rz">axis Rz (unit meter)</param>
+        /// <param name="_G">gripper pos (0~255)</param>
+        public URCoordinates(double _x, double _y, double _z, double _Rx, double _Ry, double _Rz, byte _G = 0)
+        {
+            X = new Unit(_x);
+            Y = new Unit(_y);
+            Z = new Unit(_z);
+            Rx = new Unit(_Rx);
+            Ry = new Unit(_Ry);
+            Rz = new Unit(_Rz);
+            Grip = _G;
+        }
+        public URCoordinates(URCoordinates input)
+        {
+            X = input.X;
+            Y = input.Y;
+            Z = input.Z;
+            Rx = input.Rx;
+            Ry = input.Ry;
+            Rz = input.Rz;
+        }
+
+        /// <summary>
+        /// e.g.
+        /// "p[]" → p[X,Y,Z,Rx,Ry,Rz]
+        /// "[]" → [X,Y,Z,Rx,Ry,Rz]
+        /// "()" → (X,Y,Z,Rx,Ry,Rz)
+        /// "(3)" → (X,Y,Z)
+        /// "[7]" → [X,Y,Z,Rx,Ry,Rz,Grip]
+        /// </summary>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public string ToString(string format = "[]")
+        {
+            string rtn = "";
+            if (format.IndexOf('p') >= 0)
+                rtn += "p";
+            if (format.IndexOf('[') >= 0)
+                rtn += "[";
+            if (format.IndexOf('(') >= 0)
+                rtn += "(";
+
+            if (format.IndexOf('3') >= 0)
+                rtn += $"{X.M},{Y.M},{Z.M}";
+            else if(format.IndexOf('6') >= 0)
+                rtn += $"{X.M},{Y.M},{Z.M},{Rx.M},{Ry.M},{Rz.M}";
+            else if (format.IndexOf('7') >= 0)
+                rtn += $"{X.M},{Y.M},{Z.M},{Rx.M},{Ry.M},{Rz.M},{Grip}";
+            else
+                rtn += $"{X.M},{Y.M},{Z.M},{Rx.M},{Ry.M},{Rz.M}";
+
+            if (format.IndexOf(']') >= 0)
+                rtn += "]";
+            if (format.IndexOf(')') >= 0)
+                rtn += ")";
+
+            return rtn;
+
+        }
+        //public string ToString(string unit = "m", string type = "[", string format = null)
+        //{
+        //    if (unit == "mm")
+        //    {
+        //        X *= 1000f;
+        //        Y *= 1000f;
+        //        Z *= 1000f;
+        //    }
+        //    string x = X.ToString(format);
+        //    string y = Y.ToString(format);
+        //    string z = Z.ToString(format);
+        //    string rx = Rx.ToString(format);
+        //    string ry = Ry.ToString(format);
+        //    string rz = Rz.ToString(format);
+        //    if (type == "[")
+        //        return $"[{x},{y},{z},{rx},{ry},{rz}]";
+        //    else if (type == "(")
+        //        return $"({x},{y},{z},{rx},{ry},{rz})";
+        //    else if (type == "3(")
+        //        return $"({x},{y},{z})";
+        //    else if (type == "3[")
+        //        return $"[{x},{y},{z}]";
+        //    else
+        //        return $"[{x},{y},{z},{rx},{ry},{rz}]";
+        //}
+
+    }
+}
---
+++ PC/CS_UrRobot/UrSocketControl.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using UrRobot.Coordinates;
+
+namespace UrRobot.Socket
+{
+    public enum mode
+    {
+        End = -1,
+        stop = 0,
+        recordj = 1,
+        recordp = 2,
+        jmovej = 3,
+        pmovep = 4,
+        gripper = 5,
+        grip = 6,
+        jog = 10,
+        moveByFile = 99
+    }
+    class UrSocketControl
+    {
+        public mode cmd = mode.stop;
+
+        static bool serverOn = false;
+        public bool isServerRunning
+        {
+            get { return serverOn; }
+        }
+
+        #region  //---Server---//
+        Thread thread_server;
+        public void startServer(string ip, int port)
+        {
+            if (serverOn) // is the server is on, return
+            { Console.WriteLine("已經在執行了"); return; }
+
+            IPAddress IPAddress = IPAddress.Parse(ip);
+            TcpListener tcpListener = new TcpListener(IPAddress, port);
+            thread_server = new Thread(() => Server(tcpListener, ref cmd));
+            thread_server.Start();
+        }
+        private void Server(TcpListener tcp, ref mode cmd)
+        {
+            //creat tcp
+            try
+            {
+                tcp.Start();
+                //OnLinkState(new LinkArgs("Wait Connect"));
+            }
+            catch (Exception ex) { Console.WriteLine("tcp start error\n" + ex); return; }
+
+            TcpClient UR_Client = tcp.AcceptTcpClient();
+            NetworkStream stream;
+            if (UR_Client.Client.Connected)
+            {
+                string sMsg = "";
+                stream = UR_Client.GetStream();
+                stream.WriteTimeout = 100;
+                //OnLinkState(new LinkArgs("Connect"));
+                serverOn = true;
+                cmd = mode.stop;
+                while (serverOn)
+                {
+                    if (cmd == mode.moveByFile)
+                    {
+                        string[] fileList = System.IO.File.ReadAllLines(ReadingFile);
+                        for (int p = 0; p < fileList.Count();)
+                        {
+                            if (p >= fileList.Count())//代表最後一行了
+                                break;
+                            if (fileList[p] == "")
+                            { p++; continue; }
+
+                            switch (fileList[p])
+                            {
+                                case "position":
+                                    while (true)
+                                    {
+                                        p++;
+                                        if (p >= fileList.Count())//代表最後一行了
+                                            break;
+                                        if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                            break;
+                                        _sendMsg("pmovep", ref cmd);//record pos模式
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得 movep
+
+                                        byte[] _pcount = new byte[1] { 1 };
+                                        stream.Write(_pcount, 0, 1);//@test   1個點
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"set" 也就是要開始給座標點
+
+                                        _sendMsg("(" + fileList[p].Substring(2, fileList[p].Length - 3) + ")", ref cmd);//point
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+                                    }
+                                    break;
+                                case "joint":
+                                    while (true)
+                                    {
+                                        p++;
+                                        if (p >= fileList.Count())//代表最後一行了
+                                            break;
+                                        if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                            break;
+                                        _sendMsg("jmovej", ref cmd);//jmovej模式
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得 movej
+
+                                        byte[] _pcount = new byte[1] { 1 };
+                                        stream.Write(_pcount, 0, 1);//@test   1個點
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"set" 也就是要開始給座標點
+
+                                        _sendMsg("(" + fileList[p].Substring(1, fileList[p].Length - 2) + ")", ref cmd);//joint
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+                                    }
+                                    break;
+                                case "Rmovej":
+                                    while (true)
+                                    {
+                                        p++;
+                                        if (p >= fileList.Count())//代表最後一行了
+                                            break;
+                                        if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                            break;
+                                        _sendMsg("Rmovej", ref cmd);//Rmovej模式 //相對移動 joint
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得 Rmovej
+
+                                        byte[] _pcount = new byte[1] { 1 };
+                                        stream.Write(_pcount, 0, 1);//@test   1個點
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"set" 也就是要開始給座標點
+
+                                        _sendMsg("(" + fileList[p].Substring(1, fileList[p].Length - 2) + ")", ref cmd);
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+                                    }
+                                    break;
+                                case "Rmovep":
+                                    while (true)
+                                    {
+                                        p++;
+                                        if (p >= fileList.Count())//代表最後一行了
+                                            break;
+                                        if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                            break;
+                                        _sendMsg("Rmovep", ref cmd);//Rmovep模式
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得 Rmovej
+
+                                        byte[] _pcount = new byte[1] { 1 };
+                                        stream.Write(_pcount, 0, 1);//@test   1個點
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"set" 也就是要開始給座標點
+
+                                        _sendMsg("(" + fileList[p].Substring(2, fileList[p].Length - 3) + ")", ref cmd);//point
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+                                    }
+                                    break;
+                                case "pmovej":
+                                    while (true)
+                                    {
+                                        p++;
+                                        if (p >= fileList.Count())//代表最後一行了
+                                            break;
+                                        if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                            break;
+                                        _sendMsg("pmovej", ref cmd);//record pos模式
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得 movep
+
+                                        byte[] _pcount = new byte[1] { 1 };
+                                        stream.Write(_pcount, 0, 1);//@test   1個點
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"set" 也就是要開始給座標點
+
+                                        _sendMsg("(" + fileList[p].Substring(2, fileList[p].Length - 3) + ")", ref cmd);//point
+
+                                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                        Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+                                    }
+                                    break;
+                                case "gripper"://gripper 只能一行 gripper 一行 數字
+                                    p++;
+                                    _sendMsg("gripper", ref cmd);//gripper模式
+
+                                    sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                    Console.WriteLine("Robot : " + sMsg);//應該會是獲得 gripper
+
+                                    byte[] pcount = new byte[3] { (byte)fileList[p].toInt(), 0, 150 };
+                                    stream.Write(pcount, 0, 3);//pos force speed
+                                                               //_sendMsg("(" + rq_pos + "," + 0 + "," + 150 + ")");
+
+                                    sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                    Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+
+                                    p++;
+                                    break;
+                                case "sleep":
+                                    p++;
+                                    Thread.Sleep(fileList[p].toInt());
+                                    p++;
+                                    break;
+
+                                case "test":
+                                    p++;
+                                    if (p >= fileList.Count())//代表最後一行了
+                                        break;
+                                    if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                        break;
+                                    _sendMsg("test", ref cmd);
+
+                                    List<string> servoCmd = new List<string>();
+                                    servoCmd.Add(fileList[p]);
+                                    while (true)
+                                    {
+                                        p++;
+                                        if (p >= fileList.Count())//代表最後一行了
+                                            break;
+                                        if (fileList[p].IndexOf('[') == -1)//代表是指令
+                                            break;
+                                        servoCmd.Add(fileList[p]);
+                                    }
+
+                                    sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                    Console.WriteLine("Robot : " + sMsg);//應該會是獲得 test
+
+                                    for (int i = 0; i < servoCmd.Count; i++)
+                                    {
+                                        string str = $"( {servoCmd[i].Substring(1, servoCmd[i].Length - 2)})";
+                                        _sendMsg(str, ref cmd);
+                                    }
+                                    _sendMsg("(0,0,-100,0,0,0)", ref cmd);
+
+                                    sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                                    Console.WriteLine("Robot : " + sMsg);//應該會是獲得"work done" 
+                                    break;
+                            }
+                            if (sMsg == "End") break;
+                        }
+                        cmd = mode.stop;
+                    }
+
+                    else if (cmd == mode.stop)
+                    {
+                        while (cmd == mode.stop)
+                        {
+                            if (!_sendMsg("stop", ref cmd)) break;
+                            sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                            Console.WriteLine(sMsg); //拿到
+                            if (sMsg != "UR:stop")
+                                Console.WriteLine("error!! UR robot didn't stop?");
+                        }
+                    }
+                    else if (cmd == mode.recordj)
+                    {
+                        if (!_sendMsg("recordj", ref cmd)) break;
+                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                        Console.WriteLine("UR : " + sMsg);//應該會是獲得 robot joint
+                    }
+                    else if (cmd == mode.recordp)//遺棄 (因為 position 轉 joint 會出問題，錄製position很常無法執行
+                    {
+                    }
+                    else if (cmd == mode.jog)//未完成
+                    {
+                    }
+                    else if (cmd == mode.gripper)
+                    {
+                        if (!_sendMsg("gripper", ref cmd)) break;
+                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                        Console.WriteLine(sMsg);
+
+                        int rq_pos = 0;
+                        int rq_force = 0;
+                        int rq_speed = 10;
+                        if (!_sendMsg($"[{rq_pos},{rq_force},{rq_speed},0,0,0]", ref cmd)) break;
+
+                        sMsg = _waitRead(ref cmd); if (sMsg == "End") break;
+                        Console.WriteLine(sMsg);
+                        if (sMsg != "UR:done")
+                            Console.WriteLine("error!! UR robot didn't finish work?");
+
+                        cmd = mode.stop;
+                    }
+                    else if (cmd == mode.grip)//遺棄
+                    {
+                    }
+                    else if (cmd == mode.End)
+                    {
+                        break;
+                    }
+                }//while serverOn
+                stream.Close();
+
+            }//if client connect
+            UR_Client.Dispose();
+            tcp.Stop();
+            //OnLinkState(new LinkArgs("Disconnect"));
+            serverOn = false;
+
+            #region subfunction
+            string _waitRead(ref mode c)
+            {
+                string msg_read = "";
+                while (serverOn)
+                {
+                    byte[] arrayBytesRequest = new byte[UR_Client.Available];
+                    try
+                    {
+                        int nRead = stream.Read(arrayBytesRequest, 0, arrayBytesRequest.Length);//讀取UR手臂的數值
+                        if (nRead > 0)//確認有收到東西後
+                        {
+                            msg_read = ASCIIEncoding.ASCII.GetString(arrayBytesRequest);
+                            return msg_read;
+                        }//read>0 //所以如果沒有收到的話就繼續再收一次
+                    }
+                    catch
+                    {
+                        c = mode.End;
+                        return "End";
+                    }
+                }
+                return "End";
+            }
+            bool _sendMsg(string str, ref mode c)
+            {
+                try
+                {
+                    byte[] arrayBytesAnswer = ASCIIEncoding.ASCII.GetBytes(str);
+                    stream.Write(arrayBytesAnswer, 0, arrayBytesAnswer.Length);
+                }
+                catch
+                {
+                    //可能是client先結束了
+                    Console.WriteLine("可能是client先結束了");
+                    c = mode.End;
+                    return false;
+                }
+                return true;
+            }
+            #endregion subfunction
+        }
+
+        public void stopServer()
+        {
+            serverOn = false;
+            thread_server.Abort();
+            //OnLinkState(new LinkArgs("disconnect"));
+        }
+
+
+        #endregion //---Server---//
+        void tmp()
+        {
+            URCoordinates urc = new URCoordinates(3.2.M(), 3.2.M(), 3.2.M(), 3.2.M(), 3.2.M(), 3.2.M());
+
+        }
+    }
+}
